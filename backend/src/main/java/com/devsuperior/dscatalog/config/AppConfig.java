230125fda5e8651ca5477ed9c630fc18535213b2
miguelmilagres <miguelmdemacedo@gmail.com>
2024-06-06 23:21:20 -0300

    GrantedAuthority, UserDetails, UserDetailsService

--- backend/src/main/java/com/devsuperior/dscatalog/config/AppConfig.java
+++ backend/src/main/java/com/devsuperior/dscatalog/config/AppConfig.java
@@ -2,26 +2,14 @@ package com.devsuperior.dscatalog.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 public class AppConfig {
 
 	@Bean
-	public BCryptPasswordEncoder passwordEncoder() {
+	PasswordEncoder passwordEncoder() {
 		return new BCryptPasswordEncoder();
-	}
-	
-	@Configuration
-	public class SecurityConfig {
-
-		@Bean
-		public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-			http.csrf(csrf -> csrf.disable());
-			http.authorizeHttpRequests(auth -> auth.anyRequest().permitAll());
-			return http.build();
-		}
 	}
 }
--- backend/src/main/java/com/devsuperior/dscatalog/entities/User.java
+++ backend/src/main/java/com/devsuperior/dscatalog/entities/User.java
@@ -1,8 +1,12 @@
 package com.devsuperior.dscatalog.entities;
 
+import java.util.Collection;
 import java.util.HashSet;
 import java.util.Objects;
 import java.util.Set;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
@@ -14,9 +18,10 @@ import jakarta.persistence.JoinTable;
 import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
 
+@SuppressWarnings("serial")
 @Entity
 @Table(name = "tb_user")
-public class User {
+public class User implements UserDetails {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -90,6 +95,19 @@ public class User {
 		return roles;
 	}
 	
+	public void addRole(Role role) {
+    	roles.add(role);
+    }
+    
+	public boolean hasRole(String roleName) {
+		for (Role role : roles) {
+			if (role.getAuthority().equals(roleName)) {
+				return true;
+			}
+		}
+		return false;
+	}
+	
 	@Override
 	public int hashCode() {
 		return Objects.hash(id);
@@ -105,5 +123,35 @@ public class User {
 			return false;
 		User other = (User) obj;
 		return Objects.equals(id, other.id);
+	}
+
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		return roles;
+	}
+
+	@Override
+	public String getUsername() {
+		return email;
+	}
+
+	@Override
+	public boolean isAccountNonExpired() {
+		return true;
+	}
+
+	@Override
+	public boolean isAccountNonLocked() {
+		return true;
+	}
+
+	@Override
+	public boolean isCredentialsNonExpired() {
+		return true;
+	}
+
+	@Override
+	public boolean isEnabled() {
+		return true;
 	}
 }
